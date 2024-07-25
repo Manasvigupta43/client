2774ee2f42b31cc2e6c3c6945f657bfb679f7ecd
Manasvi Gupta <mgupta@academian.com>
2024-07-26 00:09:40 +0530

    main

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css'; // Import the CSS file
+ 
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <p>© 2024 Academian. All rights reserved.</p>
+        <nav className="footer-nav">
+          <a href="#privacy">Privacy Policy</a>
+          <a href="#terms">Terms of Service</a>
+          <a href="#contact">Contact Us</a>
+          <a href="#contact">pppppppp Us</a>
+        </nav>
+      </div>
+    </footer>
+  );
+}
+ 
+export default Footer;
